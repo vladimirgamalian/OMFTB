441c8b85d5448dee3bbdf1835b6d80c1b3d0ca1a
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2016-04-13 10:21:30 +0700

    Add simple echo functionality

--- omftb.py
+++ omftb.py
@@ -9,11 +9,42 @@ import click
 BOT_API_URL = 'https://api.telegram.org/bot'
 
 
+def process_message(text):
+    return 'what is "' + text + '"?'
+
+
+def send_message(token, chat_id, text):
+    req = {'chat_id': chat_id, 'text': text}
+    requests.post(BOT_API_URL + token + '/sendMessage', json=req)
+
+
+def process_updates(token, updates):
+    if 'message' not in updates:
+        return
+    message = updates['message']
+    if 'text' not in message:
+        return
+    text = message['text']
+    chat = message['chat']
+    chat_id = chat['id']
+    send_message(token, chat_id, process_message(text))
+
+
 @click.command()
 @click.argument('token')
 def main(token):
-    r = requests.post(BOT_API_URL + token + '/getUpdates', json={'timeout': 1})
-    print r.json()
+    update_id = None
+    while True:
+        req = {'timeout': 5, 'limit': 1}
+        if update_id is not None:
+            req['offset'] = update_id
+        response = requests.post(BOT_API_URL + token + '/getUpdates', json=req).json()
+        result_array = response['result']
+        if not result_array:
+            continue
+        result = result_array[0]
+        process_updates(token, result)
+        update_id = result['update_id'] + 1
 
 
 if __name__ == '__main__':
